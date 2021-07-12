5538e1db7c0cf0823ab5f0bf455700110ed6008f
fly <2298408812@qq.com>
2021-07-13 00:20:07 +0800

    将个人信息的矩形封装通过代码实现修改

--- app/src/main/java/com/example/bigworks/persondata/PersonDataPageActivity.java
+++ app/src/main/java/com/example/bigworks/persondata/PersonDataPageActivity.java
@@ -15,9 +15,28 @@ public class PersonDataPageActivity extends AppCompatActivity {
     private View view;
     private void initElement(){
         //返回按钮back=(TextView)
-        TextView name = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.include_text).findViewById(R.id.rectangles_style_text_name);
-        name.setText("test");
         back=findViewById(R.id.layout_titlebar).findViewById(R.id.titlebar_combar_back);
+
+        TextView name = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_name).findViewById(R.id.rectangles_style_public_text);
+        TextView myName = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_name).findViewById(R.id.rectangles_style_personal_text);
+        name.setText("昵称");
+        myName.setText("小菜鸟");
+
+        TextView number = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_number).findViewById(R.id.rectangles_style_public_text);
+        TextView myNumber = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_number).findViewById(R.id.rectangles_style_personal_text);
+        number.setText("图享号");
+        myNumber.setText("1901420313");
+
+        TextView sex = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_sex).findViewById(R.id.rectangles_style_public_text);
+        TextView mySex = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_sex).findViewById(R.id.rectangles_style_personal_text);
+        number.setText("性别");
+        mySex.setText("男");
+
+        TextView signature = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_signature).findViewById(R.id.rectangles_style_public_text);
+        TextView mySignature = (TextView) findViewById(R.id.rectangles_linearlayout).findViewById(R.id.rectangles_signature).findViewById(R.id.rectangles_style_personal_text);
+        number.setText("个性签名");
+        mySignature.setText("Slago Inc");
+
     }
     private void binActionForElement(){
         back.setClickable(true);//设置为textview可点击的
