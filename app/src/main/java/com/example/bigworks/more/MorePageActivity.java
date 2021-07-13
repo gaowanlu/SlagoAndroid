cdf459c07b32b0a98801ec269296f76fb68ed3ae
gaowanlu <2209120827@qq.com>
2021-07-13 22:03:07 +0800

    增加登陆页面

--- app/src/main/java/com/example/bigworks/more/MorePageActivity.java
+++ app/src/main/java/com/example/bigworks/more/MorePageActivity.java
@@ -2,20 +2,25 @@ package com.example.bigworks.more;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.bigworks.R;
+import com.example.bigworks.login.LoginActivity;
 
 public class MorePageActivity extends AppCompatActivity {
     private View back;
     private TextView titlebar_title;
+    private TextView logout;
     private void initElement(){
         //返回按钮back=(TextView)
         back=findViewById(R.id.layout_titlebar).findViewById(R.id.titlebar_combar_back);
         //获取标题栏标题
         titlebar_title=findViewById(R.id.titlebar_title);
+        //退出登录控件
+        logout=findViewById(R.id.morepage_logout);
     }
     private void bindActionForElement(){
         //设置标题栏文字
@@ -26,6 +31,15 @@ public class MorePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();//结束活动
+            }
+        });
+        //绑定退出登录事件
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //进入登录界面并，只剩登录一个活动
+                Intent intent= new Intent(MorePageActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
