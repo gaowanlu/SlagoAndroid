16b58e14b7029437bbc537dc761b640359ab5a48
fly <2298408812@qq.com>
2021-07-16 22:24:02 +0800

    滚动选择器 设置性别

--- app/src/main/java/com/example/bigworks/persondata/MySexActivity.java
+++ app/src/main/java/com/example/bigworks/persondata/MySexActivity.java
@@ -8,24 +8,40 @@ import android.widget.TextView;
 
 import com.example.bigworks.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MySexActivity extends AppCompatActivity {
     private View back;
     private TextView titlebar_title;
+    private PickerView setMySex;
     private void initElement(){
         //返回按钮back=(TextView)
         back=findViewById(R.id.layout_titlebar).findViewById(R.id.titlebar_combar_back);
         //获取标题栏标题
         titlebar_title=findViewById(R.id.titlebar_title);
 
-        TextView sex = (TextView) findViewById(R.id.rectangles_my_sex).findViewById(R.id.rectangles_style_public_text);
-        TextView mySex = (TextView) findViewById(R.id.rectangles_my_sex).findViewById(R.id.rectangles_style_personal_text);
-        sex.setText("性别");
-        mySex.setText("男");
+//        TextView sex = (TextView) findViewById(R.id.rectangles_my_sex).findViewById(R.id.rectangles_style_public_text);
+//        TextView mySex = (TextView) findViewById(R.id.rectangles_my_sex).findViewById(R.id.rectangles_style_personal_text);
+//        sex.setText("性别");
+//        mySex.setText("男");
     }
+
+    //初始化滑动选择性别
+    private void initScrollSelector() {
+        setMySex = (PickerView) findViewById(R.id.set_my_sex);
+        List<String> setSex = new ArrayList<String>();
+        setSex.add("男");
+        setSex.add("女");
+        setSex.add("保密");
+        setMySex.setData(setSex);
+    }
+
     private void bindActionForElement(){
         //设置标题栏文字
         titlebar_title.setText("性别");
         back.setClickable(true);//设置为textview可点击的
+
         //绑定标题栏内的返回按钮字体，作为返回上级事件
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +55,7 @@ public class MySexActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sex);
         initElement();
+        initScrollSelector();
         bindActionForElement();
     }
 }
